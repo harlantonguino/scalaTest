0515d8ac951844cb9c175b37aa7582faff2d3267
harlantonguino <9009541+harlantonguino@users.noreply.github.com>
2025-03-29 15:50:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ add_slider = st.slider(
 )
 
 # Add a slider to the sidebar:
-add_slider2 = st.slider(
+add_slider2 = st.sidebar.slider(
     'Select a range of values',
     0.0, 100.0, (25.0, 75.0)
 )
